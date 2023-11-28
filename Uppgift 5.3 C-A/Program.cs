6c77701b4733c40c68e569d8d682764ae0c1fe90
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 15:00:24 +0100

    Add project files.

---
+++ Uppgift 5.3 C-A/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = new string[4] { "Hur många toaletter i skolan?", "Största landet i världen?", "Bäst?", "Sämst?" };
+            string[] svar = new string[4] {"100", "ryssland", "dijar", "younis"};
+
+            Console.WriteLine("Välj en fråga, 1-4");
+            int x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    {
+                        Console.WriteLine(frågor[0]);
+                        string y = Console.ReadLine().ToLower();
+                        if (y == svar[0])
+                        {
+                            Console.WriteLine("Rätt");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel");
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine(frågor[1]);
+                        string y = Console.ReadLine().ToLower();
+                        if (y == svar[1])
+                        {
+                            Console.WriteLine("Rätt");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel");
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine(frågor[2]);
+                        string y = Console.ReadLine().ToLower();
+                        if (y == svar[2])
+                        {
+                            Console.WriteLine("Rätt");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel");
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine(frågor[3]);
+                        string y = Console.ReadLine().ToLower();
+                        if (y == svar[3])
+                        {
+                            Console.WriteLine("Rätt");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel");
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
